e1c8f7482e66400a1b461a01d23e9ef6bd14146d
MR6-TECH <pratirajwaghmare@gmail.com>
2026-03-22 16:16:13 +0530

    Add files via upload

---
+++ ao.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+#define PI 3.12
+/*macros tutorial */
+ void tech(){
+float r=5;
+float h=10;
+float areaCircle=PI*r*r;
+printf(" area of  circular garden %f",areaCircle);
+float areaCylinder=2*PI*r*r;	
+printf(" \n area of  circular garden %f",areaCylinder);	
+}
+ void main(){
+tech();
+return 0;
+}
---
+++ hw.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+/* cpp increment decrement operator*/
+main()
+{
+ 
+ int a = 20;
+ int b = 10;
+ int c = a++;
+ int d = b--;
+ 
+cout << " \n " << c << endl ;
+cout << " \n " << d << endl ;
+
+ return 0;
+}
+
+
